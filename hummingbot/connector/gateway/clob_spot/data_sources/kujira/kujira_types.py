3163b3ee2221469ad6f89d1108296a5b432f5834
Danilo Araújo Silva <danilo.silva@funttastic.com>
2023-04-18 23:54:28 +0000

    Working with the Kujira connector. WIP.

---
+++ hummingbot/connector/gateway/clob_spot/data_sources/kujira/kujira_types.py
@@ -0,0 +1,67 @@
+from pyinjective.async_client import AsyncClient
+from pyinjective.composer import Composer as ProtoMsgComposer
+from pyinjective.constant import Network
+from pyinjective.orderhash import OrderHashResponse, build_eip712_msg, hash_order
+from pyinjective.proto.exchange.injective_accounts_rpc_pb2 import StreamSubaccountBalanceResponse, SubaccountBalance
+from pyinjective.proto.exchange.injective_explorer_rpc_pb2 import GetTxByTxHashResponse, StreamTxsResponse
+from pyinjective.proto.exchange.injective_portfolio_rpc_pb2 import StreamAccountPortfolioResponse
+from pyinjective.proto.exchange.injective_spot_exchange_rpc_pb2 import (
+    MarketsResponse,
+    SpotMarketInfo,
+    SpotOrderHistory,
+    SpotTrade,
+    StreamOrderbookResponse,
+    StreamOrdersResponse,
+    StreamTradesResponse,
+    TokenMeta,
+)
+from pyinjective.proto.injective.exchange.v1beta1.exchange_pb2 import DerivativeOrder, SpotOrder
+from pyinjective.wallet import Address
+
+AsyncClient = AsyncClient
+ProtoMsgComposer = ProtoMsgComposer
+Network = Network
+OrderHashResponse = OrderHashResponse
+build_eip712_msg = build_eip712_msg
+hash_order = hash_order
+StreamSubaccountBalanceResponse = StreamSubaccountBalanceResponse
+SubaccountBalance = SubaccountBalance
+GetTxByTxHashResponse = GetTxByTxHashResponse
+StreamTxsResponse = StreamTxsResponse
+StreamAccountPortfolioResponse = StreamAccountPortfolioResponse
+MarketsResponse = MarketsResponse
+SpotMarketInfo = SpotMarketInfo
+SpotOrderHistory = SpotOrderHistory
+SpotTrade = SpotTrade
+StreamOrderbookResponse = StreamOrderbookResponse
+StreamOrdersResponse = StreamOrdersResponse
+StreamTradesResponse = StreamTradesResponse
+TokenMeta = TokenMeta
+DerivativeOrder = DerivativeOrder
+SpotOrder = SpotOrder
+Address = Address
+
+__all__ = [
+    "AsyncClient",
+    "ProtoMsgComposer",
+    "Network",
+    "OrderHashResponse",
+    "build_eip712_msg",
+    "hash_order",
+    "StreamSubaccountBalanceResponse",
+    "SubaccountBalance",
+    "GetTxByTxHashResponse",
+    "StreamTxsResponse",
+    "StreamAccountPortfolioResponse",
+    "MarketsResponse",
+    "SpotMarketInfo",
+    "SpotOrderHistory",
+    "SpotTrade",
+    "StreamOrderbookResponse",
+    "StreamOrdersResponse",
+    "StreamTradesResponse",
+    "TokenMeta",
+    "DerivativeOrder",
+    "SpotOrder",
+    "Address",
+]
