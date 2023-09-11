1ec2096fe7f0aa63b37132b12ac5c5fe8792f528
Álvaro E. F. Mota <alvaro.mota@funttastic.com>
2023-09-11 18:12:15 -0300

    Created one test case example

--- test/hummingbot/connector/gateway/clob_spot/data_sources/kujira/test_kujira_api_data_source.py
+++ test/hummingbot/connector/gateway/clob_spot/data_sources/kujira/test_kujira_api_data_source.py
@@ -1,30 +1,127 @@
-import time
-import unittest
+# import asyncio
+import json
+# import math
+# import re
 from decimal import Decimal
-from unittest.mock import AsyncMock
-
-from hummingbot.client.config.client_config_map import ClientConfigMap
-from hummingbot.client.config.config_helpers import ClientConfigAdapter
-from hummingbot.connector.connector_base import ConnectorBase
+# from typing import Any, Dict, List, Union
+from unittest.mock import AsyncMock, MagicMock, patch
+#
+# import pandas as pd
+from aioresponses import aioresponses
+#
+# from hummingbot.connector.gateway.clob_spot.data_sources.dexalot import dexalot_constants as CONSTANTS
 from hummingbot.connector.gateway.clob_spot.data_sources.kujira.kujira_api_data_source import KujiraAPIDataSource
-from hummingbot.connector.gateway.gateway_in_flight_order import GatewayInFlightOrder
-from hummingbot.connector.gateway.gateway_order_tracker import GatewayOrderTracker
-from hummingbot.connector.utils import combine_to_hb_trading_pair
+# from hummingbot.connector.gateway.clob_spot.data_sources.dexalot.dexalot_constants import HB_TO_DEXALOT_STATUS_MAP
+# from hummingbot.connector.gateway.common_types import PlaceOrderResult
+# from hummingbot.connector.gateway.gateway_in_flight_order import GatewayInFlightOrder
+from hummingbot.connector.test_support.gateway_clob_api_data_source_test import AbstractGatewayCLOBAPIDataSourceTests
+from hummingbot.connector.test_support.network_mocking_assistant import NetworkMockingAssistant
+# from hummingbot.connector.utils import split_hb_trading_pair
 from hummingbot.core.data_type.common import OrderType, TradeType
+# from hummingbot.core.data_type.in_flight_order import OrderState, OrderUpdate
+# from hummingbot.core.data_type.order_book_message import OrderBookMessage
+# from hummingbot.core.data_type.trade_fee import TradeFeeBase
+# from hummingbot.core.event.event_logger import EventLogger
 
 
-class MockConnector(ConnectorBase):
-    pass
 
+# import time
+# import unittest
+# from decimal import Decimal
+# from unittest.mock import AsyncMock
+#
+# from hummingbot.client.config.client_config_map import ClientConfigMap
+# from hummingbot.client.config.config_helpers import ClientConfigAdapter
+# from hummingbot.connector.connector_base import ConnectorBase
+# from hummingbot.connector.gateway.clob_spot.data_sources.kujira.kujira_api_data_source import KujiraAPIDataSource
+from hummingbot.connector.gateway.gateway_in_flight_order import GatewayInFlightOrder
+# from hummingbot.connector.gateway.gateway_order_tracker import GatewayOrderTracker
+from hummingbot.connector.utils import combine_to_hb_trading_pair
+# from hummingbot.core.data_type.common import OrderType, TradeType
+#
+#
+# class MockConnector(ConnectorBase):
+#     pass
+#
+#
+# class KujiraAPIDataSourceTest(unittest.TestCase):
+#     chain: str
+#     network: str
+#     trading_pair: str
+#     base: str
+#     quote: str
+#     trading_pair: str
+#     owner_address: str
+#
+#     @classmethod
+#     def setUpClass(cls) -> None:
+#         super().setUpClass()
+#         cls.chain = "kujira" # noqa: mock
+#         cls.network = "mainnet"
+#         cls.base = "KUJI" # noqa: mock
+#         cls.quote = "USK"
+#         cls.trading_pair = combine_to_hb_trading_pair(base=cls.base, quote=cls.quote)
+#         cls.owner_address = "kujira1ga9qk68ne00wfflv7y2v92epaajt59e554uulc" # noqa: mock
+#
+#     async def setUp(self) -> None:
+#         super().setUp()
+#         client_config_map = ClientConfigAdapter(hb_config=ClientConfigMap())
+#         self.initial_timestamp = time.time()
+#
+#         self.connector = MockConnector(client_config_map=ClientConfigAdapter(ClientConfigMap()))
+#         self.tracker = GatewayOrderTracker(connector=self.connector)
+#         connector_spec = {
+#             "chain": self.chain,
+#             "network": self.network,
+#             "wallet_address": self.owner_address
+#         }
+#
+#         self.data_source = KujiraAPIDataSource(
+#             trading_pairs=[self.trading_pair],
+#             connector_spec=connector_spec,
+#             client_config_map=client_config_map,
+#         )
+#
+#         self.data_source.gateway_order_tracker = self.tracker
+#
+#         await self.data_source.start()
+#
+#     async def test_place_order(self):
+#         expected_exchange_order_id = "1365802"
+#         expected_transaction_hash = "9981D7CB9F0542F9B6778149E6501EF9625C848A165D42CA94A4CC8788379562"  # noqa: mock
+#
+#         order = GatewayInFlightOrder(
+#             client_order_id="ClientOrderID",
+#             trading_pair=self.trading_pair,
+#             order_type=OrderType.LIMIT,
+#             trade_type=TradeType.BUY,
+#             creation_timestamp=self.initial_timestamp,
+#             price=Decimal(0.64),
+#             amount=Decimal(1.0),
+#             exchange_order_id=expected_exchange_order_id,
+#             creation_transaction_hash=expected_transaction_hash,
+#         )
+#
+#         self.data_source.place_order = syncMock(return_value=order)
+#
+#         request = GatewayInFlightOrder(
+#             client_order_id="ClientOrderID",
+#             trading_pair=self.trading_pair,
+#             order_type=OrderType.LIMIT,
+#             trade_type=TradeType.BUY,
+#             creation_timestamp=self.initial_timestamp,
+#             price=Decimal(0.6115),
+#             amount=Decimal(1),
+#         )
+#
+#         # result = await self.data_source.place_order(request)
+#
+#         exchange_order_id, misc_updates = await self.data_source.place_order(order=request)
+#
+#         self.assertEqual(expected_exchange_order_id, exchange_order_id)
+#         self.assertEqual({"creation_transaction_hash": expected_transaction_hash}, misc_updates)
 
-class KujiraAPIDataSourceTest(unittest.TestCase):
-    chain: str
-    network: str
-    trading_pair: str
-    base: str
-    quote: str
-    trading_pair: str
-    owner_address: str
+class KujiraAPIDataSourceTest(AbstractGatewayCLOBAPIDataSourceTests.GatewayCLOBAPIDataSourceTests):
 
     @classmethod
     def setUpClass(cls) -> None:
@@ -36,60 +133,151 @@ class KujiraAPIDataSourceTest(unittest.TestCase):
         cls.trading_pair = combine_to_hb_trading_pair(base=cls.base, quote=cls.quote)
         cls.owner_address = "kujira1ga9qk68ne00wfflv7y2v92epaajt59e554uulc" # noqa: mock
 
-    async def setUp(self) -> None:
+    def setUp(self) -> None:
+        self.mock_api = aioresponses()
+        self.mock_api.start()
+        self.mocking_assistant = NetworkMockingAssistant()
+        self.ws_connect_patch = patch("aiohttp.ClientSession.ws_connect", new_callable=AsyncMock)
+        self.ws_connect_mock = self.ws_connect_patch.start()
         super().setUp()
-        client_config_map = ClientConfigAdapter(hb_config=ClientConfigMap())
-        self.initial_timestamp = time.time()
 
-        self.connector = MockConnector(client_config_map=ClientConfigAdapter(ClientConfigMap()))
-        self.tracker = GatewayOrderTracker(connector=self.connector)
+    def tearDown(self) -> None:
+        self.mock_api.stop()
+        self.ws_connect_patch.stop()
+        super().tearDown()
+
+    def build_api_data_source(self, with_api_key: bool = True) -> KujiraAPIDataSource:
         connector_spec = {
             "chain": self.chain,
             "network": self.network,
-            "wallet_address": self.owner_address
+            "wallet_address": self.owner_address,
         }
-
-        self.data_source = KujiraAPIDataSource(
+        data_source = KujiraAPIDataSource(
             trading_pairs=[self.trading_pair],
             connector_spec=connector_spec,
-            client_config_map=client_config_map,
+            client_config_map=self.client_config_map,
+        )
+        return data_source
+
+    def exchange_symbol_for_tokens(self, base_token: str, quote_token: str) -> str:
+        exchange_trading_pair = f"{base_token}/{quote_token}"
+        return exchange_trading_pair
+
+    def configure_last_traded_price(self, trading_pair: str, last_traded_price: Decimal):
+        self.ws_connect_mock.return_value = self.mocking_assistant.create_websocket_mock()
+        resp = {"data": [{"execId": 1675046315,
+                          "price": str(self.expected_last_traded_price),
+                          "quantity": "951.14",
+                          "takerSide": 1,
+                          "ts": "2023-03-07T14:17:52.000Z"}],
+                "pair": self.exchange_trading_pair,
+                "type": "lastTrade"}
+        self.mocking_assistant.add_websocket_aiohttp_message(
+            self.ws_connect_mock.return_value, json.dumps(resp)
         )
 
-        self.data_source.gateway_order_tracker = self.tracker
+    # def test_get_last_traded_price(self):
+    #     self.configure_last_traded_price(
+    #         trading_pair=self.trading_pair, last_traded_price=self.expected_last_traded_price,
+    #     )
+    #     self.mocking_assistant.run_until_all_aiohttp_messages_delivered(
+    #         websocket_mock=self.ws_connect_mock.return_value,
+    #     )
+    #     last_trade_price = self.async_run_with_timeout(
+    #         coro=self.data_source.get_last_traded_price(trading_pair=self.trading_pair)
+    #     )
+    #
+    #     self.assertEqual(self.expected_last_traded_price, last_trade_price)
 
-        await self.data_source.start()
+    def configure_place_order_response(
+            self,
+            timestamp: float,
+            transaction_hash: str,
+            exchange_order_id: str,
+            trade_type: TradeType,
+            price: Decimal,
+            size: Decimal,
+    ):
+        super().configure_batch_order_create_response(
+            timestamp=timestamp,
+            transaction_hash=transaction_hash,
+            created_orders=[
+                GatewayInFlightOrder(
+                    client_order_id=self.expected_buy_client_order_id,
+                    trading_pair=self.trading_pair,
+                    order_type=OrderType.LIMIT,
+                    trade_type=trade_type,
+                    creation_timestamp=timestamp,
+                    price=price,
+                    amount=size,
+                    exchange_order_id=exchange_order_id,
+                    creation_transaction_hash=transaction_hash,
+                )
+            ]
+        )
 
-    async def test_place_order(self):
-        expected_exchange_order_id = "1365802"
-        expected_transaction_hash = "9981D7CB9F0542F9B6778149E6501EF9625C848A165D42CA94A4CC8788379562"  # noqa: mock
+    @patch(
+        "hummingbot.connector.gateway.clob_spot.data_sources.gateway_clob_api_data_source_base"
+        ".GatewayCLOBAPIDataSourceBase._sleep",
+        new_callable=AsyncMock,
+    )
+    def test_place_order(self, sleep_mock: AsyncMock):
+        def sleep_mock_side_effect(delay):
+            raise Exception
 
+        sleep_mock.side_effect = sleep_mock_side_effect
+
+        self.configure_place_order_response(
+            timestamp=self.initial_timestamp,
+            transaction_hash=self.expected_transaction_hash,
+            exchange_order_id=self.expected_buy_exchange_order_id,
+            trade_type=TradeType.BUY,
+            price=self.expected_buy_order_price,
+            size=self.expected_buy_order_size,
+        )
         order = GatewayInFlightOrder(
-            client_order_id="ClientOrderID",
+            client_order_id=self.expected_buy_client_order_id,
             trading_pair=self.trading_pair,
             order_type=OrderType.LIMIT,
             trade_type=TradeType.BUY,
             creation_timestamp=self.initial_timestamp,
-            price=Decimal(0.64),
-            amount=Decimal(1.0),
-            exchange_order_id=expected_exchange_order_id,
-            creation_transaction_hash=expected_transaction_hash,
+            price=self.expected_buy_order_price,
+            amount=self.expected_buy_order_size,
+        )
+        exchange_order_id, misc_updates = self.async_run_with_timeout(
+            coro=self.data_source.place_order(order=order)
         )
 
-        self.data_source.place_order = AsyncMock(return_value=order)
+        self.assertEqual({"creation_transaction_hash": self.expected_transaction_hash}, misc_updates)
 
-        request = GatewayInFlightOrder(
-            client_order_id="ClientOrderID",
-            trading_pair=self.trading_pair,
-            order_type=OrderType.LIMIT,
-            trade_type=TradeType.BUY,
-            creation_timestamp=self.initial_timestamp,
-            price=Decimal(0.6115),
-            amount=Decimal(1),
-        )
+    def test_get_last_traded_price(self):
+        pass
+    def configure_account_balances_response(self):
+        pass
 
-        # result = await self.data_source.place_order(request)
+    def configure_empty_order_fills_response(self):
+        pass
 
-        exchange_order_id, misc_updates = await self.data_source.place_order(order=request)
+    def configure_trade_fill_response(self):
+        pass
 
-        self.assertEqual(expected_exchange_order_id, exchange_order_id)
-        self.assertEqual({"creation_transaction_hash": expected_transaction_hash}, misc_updates)
+    def exchange_base(self):
+        pass
+
+    def exchange_quote(self):
+        pass
+
+    def expected_buy_exchange_order_id(self):
+        pass
+
+    def expected_sell_exchange_order_id(self):
+        pass
+
+    def get_clob_ticker_response(self):
+        pass
+
+    def get_order_status_response(self):
+        pass
+
+    def get_trading_pairs_info_response(self):
+        pass
